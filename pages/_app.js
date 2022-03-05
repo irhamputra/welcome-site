07ae914eb0413edabbec604b3851306f1fa33b68
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2022-03-05 21:55:14 +0100

    feature: add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <>
+      <DefaultSeo
+        title="Irham Putra Prasetyo"
+        description="Get to know me first by looking this site with Stories"
+        twitter={{
+          handle: "@IRHMPTRA",
+          site: "@IRHMPTRA",
+          cardType: "summary_large_image",
+        }}
+        openGraph={{
+          site_name: "Irham Putra Prasetyo",
+          url: "https://irhamputra.com",
+          title: "Irham Putra Prasetyo",
+          description:
+            "Hi! My name is Irham Putra Prasetyo. Welcome to the my personal website!",
+          images: [
+            {
+              url: "https://irhamputra.com/_next/image?url=%2Fme-computer.svg&w=750&q=75",
+              width: 800,
+              height: 600,
+              alt: "Irham Putra",
+            },
+          ],
+        }}
+      />
       <Component {...pageProps} />
-    </ChakraProvider>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,9 @@
 import * as React from "react";
-import { DefaultSeo } from "next-seo";
-import Image from "next/image";
-import { Flex } from "@chakra-ui/react";
-
-const links = [
-  "https://github.com/irhamputra",
-  "https://twitter.com/irhmptra",
-  "https://www.linkedin.com/in/muhamad-irham-prasetyo/",
-];
 
 export default function Home() {
-  const handleOpenLink = React.useCallback(() => {
-    window.open(links[Math.floor(Math.random() * links.length)], "_blank");
-  }, []);
-
   return (
-    <>
-      <DefaultSeo
-        title="Irham Putra Prasetyo"
-        description="Get to know me first by looking this site with Stories"
-        twitter={{
-          handle: "@IRHMPTRA",
-          site: "@IRHMPTRA",
-          cardType: "summary_large_image",
-        }}
-        openGraph={{
-          site_name: "Irham Putra Prasetyo",
-          url: "https://irhamputra.com",
-          title: "Irham Putra Prasetyo",
-          description:
-            "Get to know me first by looking my Instagram Stories look like",
-          images: [
-            {
-              url:
-                "https://irhamputra.com/_next/image?url=%2Fme-computer.svg&w=750&q=75",
-              width: 800,
-              height: 600,
-              alt: "Irham Putra",
-            },
-          ],
-        }}
-      />
-
-      <Flex justifyItems="center" justifyContent="center" minH="100%">
-        <Image
-          onClick={handleOpenLink}
-          src="/me-computer.svg"
-          width={700}
-          height={700}
-          alt="pic"
-        />
-      </Flex>
-    </>
+    <div>
+      <h2 className="text-6xl">Hello World</h2>
+    </div>
   );
 }
-
-export const getServerSideProps = () => {
-  return {
-    props: {},
-  };
-};
