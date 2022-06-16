3b5d0a4ada9fc2606edfdec957e21359856d5d63
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2022-06-17 00:56:56 +0200

    fix: line height

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="h-full grid content-center">
       <a href="/Resume-Muhamad-Irham-Prasetyo.pdf" download>
-        <h1 className="md:m-0 cursor-pointer text-center font-extrabold text-transparent text-3xl xl:text-8xl bg-clip-text bg-gradient-to-r from-emerald-200 to-cyan-600">
+        <h1 className="md:m-0 cursor-pointer !leading-tight text-center font-extrabold text-transparent text-3xl xl:text-8xl bg-clip-text bg-gradient-to-r from-emerald-200 to-cyan-600">
           console.log("Hello World")
         </h1>
       </a>
